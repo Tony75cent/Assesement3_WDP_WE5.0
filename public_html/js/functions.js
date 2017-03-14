e86e1acbb7100cf4a4a8900890bc07d1885b0cc7
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-03-14 12:21:46 +0000

    Finished the work on this. Just about to commit.  Then it will be uploaded to WebElevate

--- public_html/js/functions.js
+++ public_html/js/functions.js
@@ -5,10 +5,35 @@
  */
 
 $(function () {
-    $(document).on("onload",trasnsitionEffect);
+    $(document).on("click", trasnsitionEffect);
+    $(document).on("click", trasnsitionEffect2);
+
     $("#Btnsubmit").on("click", login);
-    $("#home_Carousel").on("click",goToJumbotronPage);
-    $("#carousel_Jumbotron").on("click",goToCarouselPage);
+    $("#home_Carousel").on("click", goToIndexPage);
+    $("#carousel_Jumbotron").on("click", goToCarouselPage);
+    $("#goToCarousel").on("click", goToCarouselPage);
+    $("#goToSignIn").on("click", goToSignInPage);
+    $("#goToSignInFromLandingPage").on("click", goToSignInPage);
+    $("#to-top").on("mouseover", displayMessage);
+    $("#footer_1").on("mouseover", displayImage2);
+    $("#footer_1").on("mouseout", removeImage2);
+    $("#returnToLandingPage").on("click",goToIndexPage);
+    $("#goToSignInFromIndex").on("click",goToSignInPage);
+    $("#returnToLandingPageFromLogout").on("click",goToIndexPage);
+    $("#goToLogoutFromIndex").on("click",goToLogoutPage);
+    $("#goToLogoutFromCarousel").on("click",goToLogoutPage);
+
+    var evtTst = document.getElementById('inputEmail');
+
+    /**  evtTst.addEventListener('mouseover', moveImage, false); **/
+    evtTst.addEventListener('mouseover', displayImage, false);
+    evtTst.addEventListener('mouseout', removeImage, false);
+
+    evtTst.addEventListener('mouseout', removeImage2, false);
+
+
+
+
     function login() {
         alert("In login()");
         /**  createLocalStorage();  **/
@@ -27,7 +52,7 @@ $(function () {
          $storedpassword = localStorage.getItem("password");
          
          **/
-        if ($inputEmail == $storedusername)  {
+        if ($inputEmail == $storedusername) {
             alert("B_testing@16:16");
 
 
@@ -37,7 +62,7 @@ $(function () {
             alert("C_testing@16:18");
 
             window.location = "/Assesement3_WDP_WE5/error.html";
-                        alert("D_testing@16:18");
+            alert("D_testing@16:18");
 
 
 
@@ -82,25 +107,87 @@ $(function () {
 
 
     }
-    function goToJumbotronPage(){
-       window.location.href = "/Assesement3_WDP_WE5/Jumbotron.html";
-        
+    function goToIndexPage() {
+        window.location.href = "/Assesement3_WDP_WE5/index.html";
+
     }
-    function goToCarouselPage(){
-       window.location.href = "/Assesement3_WDP_WE5/carousel.html";
-        
+    function goToCarouselPage() {
+        window.location.href = "/Assesement3_WDP_WE5/carousel.html";
+
     }
-function trasnsitionEffect(){
-    $( "#footer" ).animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"
-  }, 5000, function() {
-    // Animation complete.
-  });
     
-    
-}
+    function goToLogoutPage() {
+        window.location.href = "/Assesement3_WDP_WE5/logout.html";
+
+    }
+    function goToSignInPage(){
+                window.location.href = "/Assesement3_WDP_WE5/signin.html";
+
+    }
+
+    function trasnsitionEffect() {
+        $(".footer").animate({height: "200px"});
+
+
+    }
+    function trasnsitionEffect2() {
+        $(".footer").animate({height: "70px"});
+
+
+    }
+    function moveImage() {
+        $(".movingImage").animate({left: '250px'});
+
+
+    }
+
+    function displayMessage(evt) {
+
+        var div = document.createElement("div");
+        div.innerHTML = "Click on this and it will return the screen to the top of the page";
+        /**  div.innerHTML("Click on this and it will return the screen to the top of the page"); **/
+        document.appendChild(div);
+        var div2 = document.getElementById('msg');
+        div2.appendChild(div);
+
+    }
+    ;
+
+
+
+    function displayImage(evt) {
+
+        var img = document.createElement("img");
+        img.setAttribute("class", "movingImage");
+        img.setAttribute("src", "Images/message.PNG");
+        img.setAttribute("width", "200");
+        img.setAttribute("alt", "somethings wrong, press the F12 key and investigate");
+        var div = document.getElementById('msg');
+        div.appendChild(img);
+
+    }
+    function removeImage(evt) {
+        var div = document.getElementById('msg');
+        div.removeChild(div.lastChild);
+
+    }
+    function displayImage2(evt) {
+
+        var img = document.createElement("img");
+        /**   img.setAttribute("class", "movingImage");  **/
+        img.setAttribute("src", "Images/Re-size_the_div.PNG");
+        img.setAttribute("width", "200");
+        img.setAttribute("alt", "somethings wrong, press the F12 key and investigate");
+        var div = document.getElementById('msg2');
+        div.appendChild(img);
+
+    }
+    function removeImage2(evt) {
+        var div = document.getElementById('msg2');
+        div.removeChild(div.lastChild);
+
+    }
+
 
 
 });
